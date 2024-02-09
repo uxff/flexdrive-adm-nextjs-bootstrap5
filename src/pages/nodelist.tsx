4d1cd09c903838b0d1d62a1f63320913767b6a39
uxff <uxff@hotmail.com>
2024-02-09 16:04:06 +0800

    update form

--- src/pages/nodelist.tsx
+++ src/pages/nodelist.tsx
@@ -47,7 +47,29 @@ export default function Nodelist() {
           </Breadcrumb.Item>
           <Breadcrumb.Item active>List</Breadcrumb.Item>
         </Breadcrumb>
-        
+
+        {/* form here         */}
+        <form>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="col">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+            {/* <div className="col">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div> */}
+            <div className="col align-middle">
+              <label className="form-label">&nbsp;</label><br/>
+              <button type="submit" className="btn btn-primary" id="btnSearch">Search</button>
+            </div>
+          </div>
+        </form>
 
         <Table striped bordered hover>
           <thead>
